3b68b5177b49ccc36029adcb58ef1a04b82a9ec0
Heitor Rabello <he1tor@hotmail.com>
2020-06-06 12:45:44 -0300

    start front-end web

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Header title="Hello World!!!" />
+
+      <h1>Conteudo da aplicação</h1>
+    </div>
+  );
+}
+
+export default App;
