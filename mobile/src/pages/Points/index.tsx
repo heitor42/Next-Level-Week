b860edac0221fdce5c0f205d158be2f2399e1d55
Heitor Rabello <he1tor@hotmail.com>
2020-06-18 17:09:57 -0300

    dynamic items

--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Constants from 'expo-constants';
@@ -6,181 +7,182 @@ import MapView, { Marker } from 'react-native-maps';
 import { SvgUri } from 'react-native-svg';
 import { Feather as Icon } from '@expo/vector-icons';
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
 const Points = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    function handleNavigationToHome() {
-      navigation.goBack();
-    }
+  const [items, setItems] = useState<Item[]>([])
 
-    function handleNavigateToDetail() {
-      navigation.navigate('Detail');
-    }
+  useEffect(() => {
+    api.get('items').then(response => {
+      setItems(response.data);
+    });
+  }, []);
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity onPress={handleNavigationToHome}>
-                <Icon name='arrow-left' size={20} color='#34CB79' />
+  function handleNavigationToHome() {
+    navigation.goBack();
+  }
+
+  function handleNavigateToDetail() {
+    navigation.navigate('Detail');
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleNavigationToHome}>
+        <Icon name='arrow-left' size={20} color='#34CB79' />
+      </TouchableOpacity>
+
+      <Text style={styles.title}>Bem vindos!</Text>
+      <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+
+      <View style={styles.mapContainer}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: -23.5421559,
+            longitude: -46.6204115,
+            latitudeDelta: 0.014,
+            longitudeDelta: 0.014,
+          }}
+        >
+
+          <Marker
+            style={styles.mapMarker}
+            onPress={handleNavigateToDetail}
+            coordinate={{
+              latitude: -23.5421559,
+              longitude: -46.6204115
+            }}
+          >
+            <View style={styles.mapMarkerContainer}>
+              <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+
+              <Text style={styles.mapMarkerTitle}>Marcador</Text>
+            </View>
+          </Marker>
+        </MapView>
+
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        //contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+
+          {items.map(item => (
+            <TouchableOpacity key={item.id} style={styles.item} onPress={() => { }}>
+              <SvgUri width={42} height={42} uri={item.image_url} />
+          <Text style={styles.itemTitle}>{item.title}</Text>
             </TouchableOpacity>
-
-            <Text style={styles.title}>Bem vindos!</Text>
-            <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
-
-            
-            <View style={styles.mapContainer}>
-                <MapView 
-                  style={styles.map} 
-                  initialRegion={{
-                    latitude: -23.5421559,
-                    longitude: -46.6204115,
-                    latitudeDelta: 0.014,
-                    longitudeDelta: 0.014,
-                  }}
-                >
-
-                <Marker
-                  style={styles.mapMarker}
-                  onPress={handleNavigateToDetail}
-                  coordinate={{
-                    latitude: -23.5421559,
-                    longitude: -46.6204115
-                  }}
-                >
-                  <View style={styles.mapMarkerContainer}>
-                    <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"}} />
-
-                    <Text style={styles.mapMarkerTitle}>Marcador</Text>
-                  </View>
-                </Marker>
-                </MapView>
-
-            </View>
-            <View style={styles.itemsContainer}>
-                <ScrollView 
-                    horizontal 
-                    showsHorizontalScrollIndicator={false}
-                    //contentContainerStyle={{ paddingHorizontal: 20 }}
-                >
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.10:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpada</Text>
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.10:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpada</Text>
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.10:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpada</Text>
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity style={styles.item} onPress={() => {}}>
-                        <SvgUri width={42} height={42} uri="http://192.168.0.10:3333/uploads/lampadas.svg" />
-                        <Text style={styles.itemTitle}>Lâmpada</Text>
-                    </TouchableOpacity>
-                </ScrollView>
-            </View>
-        </View>
-    );
+          ))}
+        </ScrollView>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingHorizontal: 32,
-      paddingTop: 20 + Constants.statusBarHeight,
-    },
-  
-    title: {
-      fontSize: 20,
-      fontFamily: 'Ubuntu_700Bold',
-      marginTop: 24,
-    },
-  
-    description: {
-      color: '#6C6C80',
-      fontSize: 16,
-      marginTop: 4,
-      fontFamily: 'Roboto_400Regular',
-    },
-  
-    mapContainer: {
-      flex: 1,
-      width: '100%',
-      borderRadius: 10,
-      overflow: 'hidden',
-      marginTop: 16,
-    },
-  
-    map: {
-      width: '100%',
-      height: '100%',
-    },
-  
-    mapMarker: {
-      width: 90,
-      height: 80, 
-    },
-  
-    mapMarkerContainer: {
-      width: 90,
-      height: 70,
-      backgroundColor: '#34CB79',
-      flexDirection: 'column',
-      borderRadius: 8,
-      overflow: 'hidden',
-      alignItems: 'center'
-    },
-  
-    mapMarkerImage: {
-      width: 90,
-      height: 45,
-      resizeMode: 'cover',
-    },
-  
-    mapMarkerTitle: {
-      flex: 1,
-      fontFamily: 'Roboto_400Regular',
-      color: '#FFF',
-      fontSize: 13,
-      lineHeight: 23,
-    },
-  
-    itemsContainer: {
-      flexDirection: 'row',
-      marginTop: 16,
-      marginBottom: 32,
-    },
-  
-    item: {
-      backgroundColor: '#fff',
-      borderWidth: 2,
-      borderColor: '#eee',
-      height: 120,
-      width: 120,
-      borderRadius: 8,
-      paddingHorizontal: 16,
-      paddingTop: 20,
-      paddingBottom: 16,
-      marginRight: 8,
-      alignItems: 'center',
-      justifyContent: 'space-between',
-  
-      textAlign: 'center',
-    },
-  
-    selectedItem: {
-      borderColor: '#34CB79',
-      borderWidth: 2,
-    },
-  
-    itemTitle: {
-      fontFamily: 'Roboto_400Regular',
-      textAlign: 'center',
-      fontSize: 13,
-    },
-  });
+  container: {
+    flex: 1,
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
+  },
+
+  title: {
+    fontSize: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    marginTop: 24,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 4,
+    fontFamily: 'Roboto_400Regular',
+  },
+
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginTop: 16,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+
+  mapMarker: {
+    width: 90,
+    height: 80,
+  },
+
+  mapMarkerContainer: {
+    width: 90,
+    height: 70,
+    backgroundColor: '#34CB79',
+    flexDirection: 'column',
+    borderRadius: 8,
+    overflow: 'hidden',
+    alignItems: 'center'
+  },
+
+  mapMarkerImage: {
+    width: 90,
+    height: 45,
+    resizeMode: 'cover',
+  },
+
+  mapMarkerTitle: {
+    flex: 1,
+    fontFamily: 'Roboto_400Regular',
+    color: '#FFF',
+    fontSize: 13,
+    lineHeight: 23,
+  },
+
+  itemsContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 32,
+  },
+
+  item: {
+    backgroundColor: '#fff',
+    borderWidth: 2,
+    borderColor: '#eee',
+    height: 120,
+    width: 120,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    paddingTop: 20,
+    paddingBottom: 16,
+    marginRight: 8,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    textAlign: 'center',
+  },
+
+  selectedItem: {
+    borderColor: '#34CB79',
+    borderWidth: 2,
+  },
+
+  itemTitle: {
+    fontFamily: 'Roboto_400Regular',
+    textAlign: 'center',
+    fontSize: 13,
+  },
+});
 
 export default Points;
